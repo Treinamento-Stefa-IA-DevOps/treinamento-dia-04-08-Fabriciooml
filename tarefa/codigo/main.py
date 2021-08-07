4cee49ac0789b2693df2af0716f0775f5ff27161
Fabrício Lima <45342038+Fabriciooml@users.noreply.github.com>
2021-08-06 23:00:45 -0300

    Model predicting from POST parameters and returning a JSON with it

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -4,9 +4,17 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.post('/model')
 ## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
-    with open('model/Titanic.pkl', 'rb') as fid: 
+def titanic(Sex:int, Age:float, Lifeboat:int, Pclass:int):
+    with open('./codigo/model/Titanic.pkl', 'rb') as fid: 
         titanic = pickle.load(fid)
+
+    prediction = titanic.predict([[Sex, Age, Lifeboat, Pclass]])
+    
+    prediction = bool(prediction)
+    
+    return {"survived": prediction, 
+            "status": 200,
+            "message": "SUCCESS"}
 
 @app.get('/model')
 def get():
